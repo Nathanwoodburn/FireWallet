092e30799b946b17228bb88dd2c13c7e392ac089
Nathan Woodburn <github@nathan.woodburn.au>
2023-06-12 21:03:54 +1000

    main: Added TX windows to open and view txs in

---
+++ FireWallet/TXForm.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace FireWallet
+{
+    partial class TXForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(TXForm));
+            labelHash = new Label();
+            groupBoxInputs = new GroupBox();
+            groupBoxOutputs = new GroupBox();
+            buttonExplorer = new Button();
+            panelInputs = new Panel();
+            panelOutputs = new Panel();
+            groupBoxInputs.SuspendLayout();
+            groupBoxOutputs.SuspendLayout();
+            SuspendLayout();
+            // 
+            // labelHash
+            // 
+            labelHash.AutoSize = true;
+            labelHash.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            labelHash.Location = new Point(12, 9);
+            labelHash.Name = "labelHash";
+            labelHash.Size = new Size(48, 21);
+            labelHash.TabIndex = 0;
+            labelHash.Text = "Hash:";
+            // 
+            // groupBoxInputs
+            // 
+            groupBoxInputs.Controls.Add(panelInputs);
+            groupBoxInputs.Location = new Point(12, 60);
+            groupBoxInputs.Name = "groupBoxInputs";
+            groupBoxInputs.Size = new Size(277, 378);
+            groupBoxInputs.TabIndex = 1;
+            groupBoxInputs.TabStop = false;
+            groupBoxInputs.Text = "Inputs";
+            // 
+            // groupBoxOutputs
+            // 
+            groupBoxOutputs.Controls.Add(panelOutputs);
+            groupBoxOutputs.Location = new Point(295, 60);
+            groupBoxOutputs.Name = "groupBoxOutputs";
+            groupBoxOutputs.Size = new Size(493, 378);
+            groupBoxOutputs.TabIndex = 0;
+            groupBoxOutputs.TabStop = false;
+            groupBoxOutputs.Text = "Outputs";
+            // 
+            // buttonExplorer
+            // 
+            buttonExplorer.FlatStyle = FlatStyle.Flat;
+            buttonExplorer.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            buttonExplorer.Location = new Point(699, 12);
+            buttonExplorer.Name = "buttonExplorer";
+            buttonExplorer.Size = new Size(89, 37);
+            buttonExplorer.TabIndex = 0;
+            buttonExplorer.Text = "Explorer";
+            buttonExplorer.UseVisualStyleBackColor = true;
+            buttonExplorer.Click += Explorer_Click;
+            // 
+            // panelInputs
+            // 
+            panelInputs.AutoScroll = true;
+            panelInputs.Dock = DockStyle.Fill;
+            panelInputs.Location = new Point(3, 19);
+            panelInputs.Name = "panelInputs";
+            panelInputs.Size = new Size(271, 356);
+            panelInputs.TabIndex = 0;
+            // 
+            // panelOutputs
+            // 
+            panelOutputs.AutoScroll = true;
+            panelOutputs.Dock = DockStyle.Fill;
+            panelOutputs.Location = new Point(3, 19);
+            panelOutputs.Name = "panelOutputs";
+            panelOutputs.Size = new Size(487, 356);
+            panelOutputs.TabIndex = 0;
+            // 
+            // TXForm
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(buttonExplorer);
+            Controls.Add(groupBoxOutputs);
+            Controls.Add(groupBoxInputs);
+            Controls.Add(labelHash);
+            FormBorderStyle = FormBorderStyle.Fixed3D;
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            MaximizeBox = false;
+            Name = "TXForm";
+            Text = "TXForm";
+            Load += TXForm_Load;
+            groupBoxInputs.ResumeLayout(false);
+            groupBoxOutputs.ResumeLayout(false);
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label labelHash;
+        private GroupBox groupBoxInputs;
+        private GroupBox groupBoxOutputs;
+        private Button buttonExplorer;
+        private Panel panelInputs;
+        private Panel panelOutputs;
+    }
+}
---
+++ FireWallet/TXForm.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Newtonsoft.Json.Linq;
+
+namespace FireWallet
+{
+    public partial class TXForm : Form
+    {
+        MainForm mainForm;
+        JObject tx;
+        public TXForm(MainForm mainForm, JObject tx)
+        {
+            InitializeComponent();
+            // Theme
+            this.BackColor = ColorTranslator.FromHtml(mainForm.theme["background"]);
+            this.ForeColor = ColorTranslator.FromHtml(mainForm.theme["foreground"]);
+            foreach (Control c in Controls)
+            {
+                mainForm.ThemeControl(c);
+            }
+
+            this.mainForm = mainForm;
+            this.tx = tx;
+
+            this.Text = "TX: " + tx["hash"].ToString();
+        }
+
+        private void TXForm_Load(object sender, EventArgs e)
+        {
+            labelHash.Text = "Hash: " + tx["hash"].ToString();
+            mainForm.AddLog("Viewing:\n" + tx.ToString());
+            
+
+            // For each input
+            JArray inputs = (JArray)tx["inputs"];
+            foreach (JObject input in inputs)
+            {
+                Panel PanelInput = new Panel();
+                PanelInput.Size = new Size(panelInputs.Width - SystemInformation.VerticalScrollBarWidth - 10, 50);
+                PanelInput.Location = new Point(5, panelInputs.Controls.Count * 50);
+                PanelInput.BorderStyle = BorderStyle.FixedSingle;
+
+                Label address = new Label();
+                string addressString = input["address"].ToString().Substring(0,5) + "..." + input["address"].ToString().Substring(input["address"].ToString().Length - 5, 5);
+                address.Text = "Address: " + addressString;
+                address.Location = new Point(5, 5);
+                address.AutoSize = true;
+                PanelInput.Controls.Add(address);
+
+                Label amount = new Label();
+                Decimal value = Decimal.Parse(input["value"].ToString())/1000000;
+                amount.Text = "Amount: " + value.ToString();
+                amount.Location = new Point(5, 25);
+                amount.AutoSize = true;
+                PanelInput.Controls.Add(amount);
+
+                if (input["path"].ToString() != "")
+                {
+                    Label ownAddress = new Label();
+                    ownAddress.Text = "Own Address";
+                    ownAddress.Location = new Point(PanelInput.Width - 100, 5);
+                    ownAddress.AutoSize = true;
+                    PanelInput.Controls.Add(ownAddress);
+                }
+
+                panelInputs.Controls.Add(PanelInput);
+            }
+
+            // For each output
+            JArray outputs = (JArray)tx["outputs"];
+            foreach (JObject output in outputs)
+            {
+                Panel PanelOutput = new Panel();
+                PanelOutput.Size = new Size(panelOutputs.Width - SystemInformation.VerticalScrollBarWidth - 10, 50);
+                PanelOutput.Location = new Point(5, panelOutputs.Controls.Count * 50);
+                PanelOutput.BorderStyle = BorderStyle.FixedSingle;
+
+                Label address = new Label();
+                string addressString = output["address"].ToString().Substring(0, 5) + "..." + output["address"].ToString().Substring(output["address"].ToString().Length - 5, 5);
+                address.Text = "Address: " + addressString;
+                address.Location = new Point(5, 5);
+                address.AutoSize = true;
+                PanelOutput.Controls.Add(address);
+
+                JObject covenant = (JObject)output["covenant"];
+                if (covenant.ContainsKey("action"))
+                {
+                    Label covenantLabel = new Label();
+                    covenantLabel.Text = "Type: " + covenant["action"].ToString();
+                    covenantLabel.Location = new Point(5, 25);
+                    covenantLabel.AutoSize = true;
+                    PanelOutput.Controls.Add(covenantLabel);
+                }
+
+                Label amount = new Label();
+                Decimal value = Decimal.Parse(output["value"].ToString()) / 1000000;
+                amount.Text = "Amount: " + value.ToString();
+                amount.Location = new Point(PanelOutput.Width - 100, 25);
+                amount.AutoSize = true;
+                PanelOutput.Controls.Add(amount);
+
+                if (output["path"].ToString() != "")
+                {
+                    Label ownAddress = new Label();
+                    ownAddress.Text = "Own Address";
+                    ownAddress.Location = new Point(PanelOutput.Width - 100, 5);
+                    ownAddress.AutoSize = true;
+                    PanelOutput.Controls.Add(ownAddress);
+                }
+
+                panelOutputs.Controls.Add(PanelOutput);
+            }
+        }
+
+        private void Explorer_Click(object sender, EventArgs e)
+        {
+            // Open the transaction in a browser
+            string url = mainForm.userSettings["explorer-tx"] + tx["hash"].ToString();
+            ProcessStartInfo psi = new ProcessStartInfo
+            {
+                FileName = url,
+                UseShellExecute = true
+            };
+            Process.Start(psi);
+        }
+    }
+}
