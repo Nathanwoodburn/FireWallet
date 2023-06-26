5be4f0d6e91bf94afd652a8b3540e66b8cc001fb
Nathan Woodburn <github@nathan.woodburn.au>
2023-06-26 16:21:05 +1000

    multisig: Added import tx

---
+++ FireWallet/ImportTXForm.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace FireWallet
+{
+    partial class ImportTXForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ImportTXForm));
+            groupBoxIn = new GroupBox();
+            panelIn = new Panel();
+            groupBoxOut = new GroupBox();
+            panelOut = new Panel();
+            buttonSign = new Button();
+            Cancelbutton2 = new Button();
+            groupBoxIn.SuspendLayout();
+            groupBoxOut.SuspendLayout();
+            SuspendLayout();
+            // 
+            // groupBoxIn
+            // 
+            groupBoxIn.Controls.Add(panelIn);
+            groupBoxIn.Location = new Point(12, 83);
+            groupBoxIn.Name = "groupBoxIn";
+            groupBoxIn.Size = new Size(341, 355);
+            groupBoxIn.TabIndex = 3;
+            groupBoxIn.TabStop = false;
+            groupBoxIn.Text = "Inputs";
+            // 
+            // panelIn
+            // 
+            panelIn.AutoScroll = true;
+            panelIn.Dock = DockStyle.Fill;
+            panelIn.Location = new Point(3, 19);
+            panelIn.Name = "panelIn";
+            panelIn.Size = new Size(335, 333);
+            panelIn.TabIndex = 0;
+            // 
+            // groupBoxOut
+            // 
+            groupBoxOut.Controls.Add(panelOut);
+            groupBoxOut.Location = new Point(359, 83);
+            groupBoxOut.Name = "groupBoxOut";
+            groupBoxOut.Size = new Size(429, 355);
+            groupBoxOut.TabIndex = 0;
+            groupBoxOut.TabStop = false;
+            groupBoxOut.Text = "Outputs";
+            // 
+            // panelOut
+            // 
+            panelOut.AutoScroll = true;
+            panelOut.Dock = DockStyle.Fill;
+            panelOut.Location = new Point(3, 19);
+            panelOut.Name = "panelOut";
+            panelOut.Size = new Size(423, 333);
+            panelOut.TabIndex = 0;
+            // 
+            // buttonSign
+            // 
+            buttonSign.FlatStyle = FlatStyle.Flat;
+            buttonSign.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            buttonSign.Location = new Point(705, 444);
+            buttonSign.Name = "buttonSign";
+            buttonSign.Size = new Size(83, 36);
+            buttonSign.TabIndex = 2;
+            buttonSign.Text = "Sign";
+            buttonSign.UseVisualStyleBackColor = true;
+            // 
+            // Cancelbutton2
+            // 
+            Cancelbutton2.FlatStyle = FlatStyle.Flat;
+            Cancelbutton2.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            Cancelbutton2.Location = new Point(616, 444);
+            Cancelbutton2.Name = "Cancelbutton2";
+            Cancelbutton2.Size = new Size(83, 36);
+            Cancelbutton2.TabIndex = 2;
+            Cancelbutton2.Text = "Cancel";
+            Cancelbutton2.UseVisualStyleBackColor = true;
+            Cancelbutton2.Click += Cancelbutton2_Click;
+            // 
+            // ImportTXForm
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 485);
+            Controls.Add(groupBoxOut);
+            Controls.Add(groupBoxIn);
+            Controls.Add(Cancelbutton2);
+            Controls.Add(buttonSign);
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            MaximizeBox = false;
+            Name = "ImportTXForm";
+            Text = "ImportTXForm";
+            Load += ImportTXForm_Load;
+            groupBoxIn.ResumeLayout(false);
+            groupBoxOut.ResumeLayout(false);
+            ResumeLayout(false);
+        }
+
+        #endregion
+        private GroupBox groupBoxIn;
+        private GroupBox groupBoxOut;
+        private Button buttonSign;
+        private Button Cancelbutton2;
+        private Panel panelIn;
+        private Panel panelOut;
+    }
+}
---
+++ FireWallet/ImportTXForm.cs
@@ -0,0 +1,192 @@
+﻿using Newtonsoft.Json.Linq;
+
+namespace FireWallet
+{
+    public partial class ImportTXForm : Form
+    {
+        MainForm mainForm;
+        JObject tx;
+        public ImportTXForm(MainForm mainForm)
+        {
+            InitializeComponent();
+            this.mainForm = mainForm;
+        }
+
+        private void ImportTXForm_Load(object sender, EventArgs e)
+        {
+            // Theme
+            this.BackColor = ColorTranslator.FromHtml(mainForm.Theme["background"]);
+            this.ForeColor = ColorTranslator.FromHtml(mainForm.Theme["foreground"]);
+            foreach (Control c in Controls)
+            {
+                mainForm.ThemeControl(c);
+            }
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Transaction files (*.json)|*.json";
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                string tx = System.IO.File.ReadAllText(openFileDialog.FileName);
+                try
+                {
+                    JObject txObj = JObject.Parse(tx);
+                    this.tx = txObj;
+                    ParseTX();
+                }
+                catch
+                {
+                    NotifyForm notifyForm = new NotifyForm("Invalid transaction file.");
+                    notifyForm.ShowDialog();
+                    notifyForm.Dispose();
+                    this.Close();
+                }
+            }
+        }
+
+        private void Cancelbutton2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+        private async void ParseTX()
+        {
+            if (tx == null) this.Close();
+
+            string hex = tx["tx"].ToString();
+            string content = "{\"method\":\"decoderawtransaction\",\"params\":[\"" + hex + "\"]}";
+            string response = await mainForm.APIPost("", false, content);
+            if (response == null)
+            {
+                NotifyForm notifyForm = new NotifyForm("Error decoding transaction");
+                notifyForm.ShowDialog();
+                notifyForm.Dispose();
+                return;
+            }
+            JObject json = JObject.Parse(response);
+            if (json["error"].ToString() != "")
+            {
+                NotifyForm notifyForm = new NotifyForm("Error decoding transaction");
+                notifyForm.ShowDialog();
+                notifyForm.Dispose();
+                return;
+            }
+            JObject metadata = (JObject)tx["metadata"];
+            JArray metaInputs = (JArray)metadata["inputs"];
+            JArray metaOutputs = (JArray)metadata["outputs"];
+
+            JArray inputs = (JArray)json["result"]["vin"];
+            JArray outputs = (JArray)json["result"]["vout"];
+            for (int i = 0; i < inputs.Count; i++)
+            {
+                JObject input = (JObject)inputs[i];
+                JObject metaInput = (JObject)metaInputs[i];
+
+                Panel PanelInput = new Panel();
+                PanelInput.Size = new Size(panelIn.Width - SystemInformation.VerticalScrollBarWidth - 10, 50);
+                PanelInput.Location = new Point(5, panelIn.Controls.Count * 50);
+                PanelInput.BorderStyle = BorderStyle.FixedSingle;
+
+                Label txid = new Label();
+                txid.Text = "TXID: " + input["txid"].ToString();
+                txid.Location = new Point(5, 5);
+                txid.AutoSize = true;
+                PanelInput.Controls.Add(txid);
+
+                if (metaInput.ContainsKey("sighashType"))
+                {
+                    Label sighashType = new Label();
+                    sighashType.Text = "Sighash Type: " + metaInput["sighashType"].ToString();
+                    sighashType.Location = new Point(5, 25);
+                    sighashType.AutoSize = true;
+                    PanelInput.Controls.Add(sighashType);
+                }
+
+                //Label address = new Label();
+                //string addressString = input["address"].ToString().Substring(0, 5) + "..." + input["address"].ToString().Substring(input["address"].ToString().Length - 5, 5);
+                //address.Text = "Address: " + addressString;
+                //address.Location = new Point(5, 5);
+                //address.AutoSize = true;
+                //PanelInput.Controls.Add(address);
+
+                //Label amount = new Label();
+                //Decimal value = Decimal.Parse(input["value"].ToString()) / 1000000;
+                //amount.Text = "Amount: " + value.ToString();
+                //amount.Location = new Point(5, 25);
+                //amount.AutoSize = true;
+                //PanelInput.Controls.Add(amount);
+
+                //if (input["path"].ToString() != "")
+                //{
+                //    Label ownAddress = new Label();
+                //    ownAddress.Text = "Own Address";
+                //    ownAddress.Location = new Point(PanelInput.Width - 100, 5);
+                //    ownAddress.AutoSize = true;
+                //    PanelInput.Controls.Add(ownAddress);
+                //}
+
+                panelIn.Controls.Add(PanelInput);
+            }
+
+            for (int i = 0; i < outputs.Count; i++)
+            {
+                JObject output = (JObject)outputs[i];
+                JObject metaOutput = (JObject)metaOutputs[i];
+                
+                Panel PanelOutput = new Panel();
+                PanelOutput.Size = new Size(panelOut.Width - SystemInformation.VerticalScrollBarWidth - 10, 50);
+                PanelOutput.Location = new Point(5, panelOut.Controls.Count * 50);
+                PanelOutput.BorderStyle = BorderStyle.FixedSingle;
+
+                Label address = new Label();
+
+                JObject addressRaw = (JObject)output["address"];
+
+                string addressString = addressRaw["string"].ToString().Substring(0, 5) + "..." + addressRaw["string"].ToString().Substring(addressRaw["string"].ToString().Length - 5, 5);
+                address.Text = "Address: " + addressString;
+                address.Location = new Point(5, 5);
+                address.AutoSize = true;
+                PanelOutput.Controls.Add(address);
+
+                JObject covenant = (JObject)output["covenant"];
+                if (covenant.ContainsKey("action"))
+                {
+                    if (covenant["action"].ToString() != "NONE")
+                    {
+                        if (metaOutput.ContainsKey("name"))
+                        {
+
+
+
+                            Label covenantLabel = new Label();
+                            string name = metaOutput["name"].ToString();
+
+                            covenantLabel.Text = covenant["action"].ToString() + ": " + name;
+                            covenantLabel.Location = new Point(5, 25);
+                            covenantLabel.AutoSize = true;
+                            PanelOutput.Controls.Add(covenantLabel);
+                        }
+                    }
+                }
+
+                bool own = false;
+                string addressResp = await mainForm.APIGet("wallet/" + mainForm.Account + "/key/" + addressRaw["string"].ToString(),true);
+                if (addressResp != "Error") own = true;
+
+                if (own)
+                {
+                    Label ownAddress = new Label();
+                    ownAddress.Text = "Own Address";
+                    ownAddress.Location = new Point(PanelOutput.Width - 100, 5);
+                    ownAddress.AutoSize = true;
+                    PanelOutput.Controls.Add(ownAddress);
+                }
+
+                Label amount = new Label();
+                Decimal value = Decimal.Parse(output["value"].ToString());
+                amount.Text = "Amount: " + value.ToString();
+                amount.Location = new Point(PanelOutput.Width - 100, 25);
+                amount.AutoSize = true;
+                PanelOutput.Controls.Add(amount);
+                panelOut.Controls.Add(PanelOutput);
+            }
+        }
+    }
+}
